afeb1fce67af7c2fdc4d1e43fdf510353b8ce878
Jorge Modesto <jorgemodes@gmail.com>
2017-09-03 18:22:00 -0300

    Removed comment

--- src/main.js
+++ src/main.js
@@ -5,8 +5,3 @@ window.addEventListener('load', pageViewLoggerFactory())
 window.trackerjs = {
     identifyUser: identifyUserFactory()
 }
-//document.getElementById('tracker-user-email-form').addEventListener('submit', event => {
-    //console.log('tracker func')
-    //console.log(event)
-    //console.log(process.env.TRACKER_URL)
-//})
