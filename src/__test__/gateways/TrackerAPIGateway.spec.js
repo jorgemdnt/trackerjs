38ec76217942b02a318e361b8f2e0a1b3cec426d
Jorge Modesto <jorgemodes@gmail.com>
2017-09-03 17:10:26 -0300

    Added identifyUser method to TrackerAPIGateway implementation

--- src/__test__/gateways/TrackerAPIGateway.spec.js
+++ src/__test__/gateways/TrackerAPIGateway.spec.js
@@ -5,10 +5,6 @@ import TrackerAPIGateway from '../../gateways/TrackerAPIGateway'
 
 describe('TrackerAPIGateway', () => {
     const EMAIL = 'user@email.com'
-    const PROPERTIES = {
-        path: 'http://trackedpage.com',
-        timestamp: 1504446923512
-    }
     let gateway
     let axiosMock
 
@@ -22,12 +18,43 @@ describe('TrackerAPIGateway', () => {
     })
 
     describe('trackPageView', () => {
+        const PROPERTIES = {
+            path: 'http://trackedpage.com',
+            timestamp: 1504446923512
+        }
+
         test('Should send page view info to tracker servers', async () => {
             axiosMock
-                .onPost(process.env.TRACKER_URL + 'api/v1/page_views')
+                .onPost(process.env.TRACKER_URL + 'api/v1/page_views', {
+                    email: EMAIL,
+                    path: PROPERTIES.path,
+                    timestamp: PROPERTIES.timestamp
+                })
                 .reply(201)
 
             const response = await gateway.trackPageView(EMAIL, PROPERTIES)
+
+            expect(response.status).toEqual(201)
+        })
+    })
+
+    describe('identifyUser', () => {
+        const PAGE_VIEWS_STORED = [{
+            path: 'http://trackedpage.com',
+            timestamp: 1504446923512
+        }, {
+            path: 'http://trackedpage.com/contact',
+            timestamp: 1504446911512
+        }]
+
+        test('Should send user email along browser-stored page views', async () => {
+            axiosMock
+                .onPost(process.env.TRACKER_URL + 'api/v1/users', {
+                    email: EMAIL, page_views: PAGE_VIEWS_STORED
+                })
+                .reply(201)
+
+            const response = await gateway.identifyUser(EMAIL, PAGE_VIEWS_STORED)
 
             expect(response.status).toEqual(201)
         })
--- src/gateways/TrackerAPIGateway.js
+++ src/gateways/TrackerAPIGateway.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
 
 class TrackerAPIGateway {
-    constructor() {
-    }
-
     trackPageView(email, { path, timestamp }) {
         return axios.post(process.env.TRACKER_URL + 'api/v1/page_views', {
             email,
             path,
             timestamp
+        })
+    }
+
+    identifyUser(email, pageViewsStored) {
+        return axios.post(process.env.TRACKER_URL + 'api/v1/users', {
+            email,
+            page_views: pageViewsStored
         })
     }
 }
